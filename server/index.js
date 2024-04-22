d45c4e934538c6c0f2ebdd69a7310471d80c1334
Koustubh-lapate <koustubh.lapate@gmail.com>
2024-04-22 11:18:41 +0530

    Modified index.js and added all required npm packages

--- server/index.js
+++ server/index.js
@@ -1,20 +1,12 @@
 const express = require('express');
-const mongoose = require('mongoose');
 const cors = require('cors');
-const adminRouter = require("./routes/admin");
-const userRouter = require("./routes/user");
-
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-app.use("/admin", adminRouter)
-app.use("/user", userRouter)
-
-
-// Connect to MongoDB
-// DONT MISUSE THIS THANKYOU!!
-mongoose.connect('mongodb://localhost:27017/courses', { useNewUrlParser: true, useUnifiedTopology: true, dbName: "courses" });
+app.get('/', (req, res) => {
+   res.send('Hello World')
+})
 
 app.listen(3000, () => console.log('Server running on port 3000'));
